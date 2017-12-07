abb14ad2c3418907a3400b8d8fa322e27e50367a
Chris Foster <chriswfoster@gmail.com>
2017-12-07 14:11:09 -0600

    step 2

---
+++ app/swag/swag.component.js
@@ -0,0 +1,11 @@
+// default component structure: 
+// angular.module('APP_NAME_HERE').component('COMPONENT_NAME_HERE', {});
+angular.module('swagShop').component('swag', {
+templateUrl: 'app/swag/template.html',
+controllerAs: 'swagCtrl',
+bindings: {
+    item: '<', // I believes this means that the data flow is unidirectional.
+    action: '&', //action as a function
+actionLabel: '@' //action label is a string here
+}
+})
