3cec04880c521096bc83c6337db5b9c21dc25394
Chris Foster <chriswfoster@gmail.com>
2017-12-07 15:21:23 -0600

    Done... I guess

--- app/swag/swag.component.js
+++ app/swag/swag.component.js
@@ -1,11 +1,11 @@
-// default component structure: 
+// default component structure:
 // angular.module('APP_NAME_HERE').component('COMPONENT_NAME_HERE', {});
-angular.module('swagShop').component('swag', {
-templateUrl: 'app/swag/template.html',
-controllerAs: 'swagCtrl',
-bindings: {
-    item: '<', // I believes this means that the data flow is unidirectional.
-    action: '&', //action as a function
-actionLabel: '@' //action label is a string here
-}
+angular.module("swagShop").component("swag", {
+  templateUrl: "app/swag/swag.template.html",
+  controllerAs: "swagCtrl",
+  bindings: {
+    item: "<", // I believes this means that the data flow is unidirectional.
+    action: "&", //action as a function
+    actionLabel: "@" //action label is a string here
+  }
 })
